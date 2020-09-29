c4a9424a035e0b8c0f013fb84e9180ca2c30ca99
LiuXiangLin <liuxianglin2014@gmail.com>
2020-09-29 20:35:07 +0800

    第一次提交，初始化项目

---
+++ src/cn/liuxianglin/test/test.java
@@ -0,0 +1,18 @@
+package cn.liuxianglin.test;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LiuXiangLin
+ * @version 1.0
+ * @className test
+ * @date 2020/9/29 17:11
+ **/
+public class test {
+    public static void main(String[] args) {
+        String a = Integer.toBinaryString(5);
+        System.out.println(a);
+    }
+}
